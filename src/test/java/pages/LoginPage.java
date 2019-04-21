ab0d452f6f58d155395b75aa5656f287c9055f11
nik27090 <nik27090@mail.ru>
2019-04-21 13:35:36 +0300

    type of locators(String->By)

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -5,11 +5,11 @@ import org.openqa.selenium.WebDriver;
 
 public class LoginPage extends BasePage  {
 
-    private static final String LOGIN_LOCATOR = ".//input[@name='st.email']";
-    private static final String PASSWORD_LOCATOR = ".//input[@name='st.password']";
-    private static final String SUBMIT_LOCATOR = ".//input[@data-l='t,sign_in']";
-    private static final String LOGIN_CARD_LOCATOR = ".//div[@class='anonym_login_cnt js-login-state']";
-    private static final String REGISTRATION_LOCATOR = ".//a[@class='button-pro __sec __wide mb-3x' and contains(text(), 'Регистрация')]";
+    private static final By LOGIN_LOCATOR = By.xpath(".//input[@name='st.email']");
+    private static final By PASSWORD_LOCATOR = By.xpath(".//input[@name='st.password']");
+    private static final By SUBMIT_LOCATOR = By.xpath(".//input[@data-l='t,sign_in']");
+    private static final By LOGIN_CARD_LOCATOR = By.xpath(".//div[@class='anonym_login_cnt js-login-state']");
+    private static final By REGISTRATION_LOCATOR = By.xpath(".//a[@class='button-pro __sec __wide mb-3x' and contains(text(), 'Регистрация')]");
 
     public LoginPage(WebDriver driver) {
         super(driver);
@@ -19,18 +19,18 @@ public class LoginPage extends BasePage  {
     public UserMainPage login(String email, String password) {
         driver.get(baseUrl + "/dk?st.cmd=anonymMain&st.layer.cmd=PopLayerClose");
         this.check(driver);
-        driver.findElement(By.xpath(LOGIN_LOCATOR)).sendKeys(email);
-        driver.findElement(By.xpath(PASSWORD_LOCATOR)).sendKeys(password);
-        driver.findElement(By.xpath(SUBMIT_LOCATOR)).click();
+        driver.findElement(LOGIN_LOCATOR).sendKeys(email);
+        driver.findElement(PASSWORD_LOCATOR).sendKeys(password);
+        driver.findElement(SUBMIT_LOCATOR).click();
         return new UserMainPage(driver);
     }
 
     @Override
     void check(WebDriver driver) {
-        driver.findElement(By.xpath(LOGIN_CARD_LOCATOR));
-        driver.findElement(By.xpath(LOGIN_LOCATOR));
-        driver.findElement(By.xpath(PASSWORD_LOCATOR));
-        driver.findElement(By.xpath(SUBMIT_LOCATOR));
-        driver.findElement(By.xpath(REGISTRATION_LOCATOR));
+        driver.findElement(LOGIN_CARD_LOCATOR);
+        driver.findElement(LOGIN_LOCATOR);
+        driver.findElement(PASSWORD_LOCATOR);
+        driver.findElement(SUBMIT_LOCATOR);
+        driver.findElement(REGISTRATION_LOCATOR);
     }
 }
