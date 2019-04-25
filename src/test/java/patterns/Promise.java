68fc3343ece8e732afedbbb4ff816b26325ee081
Lizka <kuzo-liza@yandex.ru>
2019-04-26 03:46:08 +0400

    hehe, works

--- src/test/java/patterns/Promise.java
+++ src/test/java/patterns/Promise.java
@@ -8,9 +8,9 @@ import java.util.NoSuchElementException;
 public class Promise extends GroupPage{
 
     private static final By IN_GROUP = By.xpath(".//*[@class='dropdown_ac button-pro __with-arrow __arrow-svg __sec __wide' and contains(text(), 'В группе')]");
-    private static final By JOIN_GROUP = By.xpath(".//*[@class='dropdown_ac button-pro __with-arrow __arrow-svg __sec __wide' and contains(text(), 'Присоединиться')]");
-    private static final By SEND_REQUEST = By.xpath(".//*[@class='dropdown_ac button-pro __with-arrow __arrow-svg __sec __wide' and contains(text(), 'Запрос отправлен')]");
-    private static final By ADMIN = By.xpath(".//*[@class='dropdown_ac button-pro __with-arrow __arrow-svg __sec __wide' and contains(text(), 'Администратор')]");
+    private static final By JOIN_GROUP = By.xpath(".//*[@class='button-pro __wide' and contains(text(), 'Присоединиться')]");
+    private static final By SEND_REQUEST = By.xpath(".//*[@class='dropdown_ac button-pro __sec __wide __disabled' and contains(text(), 'Запрос отправлен')]");
+    private static final By ADMIN = By.xpath(".//*[@class='dropdown_ac button-pro __sec __disabled __wide' and contains(text(), 'Администратор')]");
 
     public Promise(WebDriver driver) {
         super(driver);
--- src/test/java/tests/GroupTest.java
+++ src/test/java/tests/GroupTest.java
@@ -1,14 +1,20 @@
 package tests;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.GroupPage;
 import pages.LoginPage;
 import patterns.Promise;
+import providers.ConfigFileProvider;
 
-public class GroupTest extends BaseTest {
+public class GroupTest {
+
+    protected WebDriver driver;
+    protected ConfigFileProvider config = new ConfigFileProvider("config.txt");
 
     @Before
     public void setUp(){
@@ -19,33 +25,25 @@ public class GroupTest extends BaseTest {
     @Test
     public void testMember(){
         GroupPage group = new GroupPage(driver);
-        group.openPage(config.getGroupInWichUserIsMember());
-        Promise promise = new Promise(driver);
-        promise.inGroup();
+        Assert.assertEquals(true, group.openPage(config.getGroupInWichUserIsMember()).checkUserStatus().inGroup());
     }
 
     @Test
     public void testNotMember(){
         GroupPage group = new GroupPage(driver);
-        group.openPage(config.getGroupInWichUserIsNotMember());
-        Promise promise = new Promise(driver);
-        promise.joinGroup();
+        Assert.assertEquals(true, group.openPage(config.getGroupInWichUserIsNotMember()).checkUserStatus().joinGroup());
     }
 
     @Test
     public void testAdministrator(){
         GroupPage group = new GroupPage(driver);
-        group.openPage(config.getGroupInWichUserIsAdmin());
-        Promise promise = new Promise(driver);
-        promise.admin();
+        Assert.assertEquals(true, group.openPage(config.getGroupInWichUserIsAdmin()).checkUserStatus().admin());
     }
 
     @Test
     public void testWaitingForConfirmation(){
         GroupPage group = new GroupPage(driver);
-        group.openPage(config.getGroupInWhichUserWaitingForConfirmation());
-        Promise promise = new Promise(driver);
-        promise.sendRequest();
+        Assert.assertEquals(true, group.openPage(config.getGroupInWhichUserWaitingForConfirmation()).checkUserStatus().sendRequest());
     }
 
     @After
