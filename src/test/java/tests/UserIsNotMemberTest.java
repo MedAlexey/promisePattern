ec2f030b3d1b6d735ba8e29575f3fa7499a8e226
Alexey <medvedev.lasta@yandex.ru>
2019-05-01 17:36:06 +0700

    implementation of UserIsNotMemberTest

--- src/test/java/tests/UserIsNotMemberTest.java
+++ src/test/java/tests/UserIsNotMemberTest.java
@@ -1,4 +1,31 @@
 package tests;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import pages.GroupPage;
+import pages.LoginPage;
+
 public class UserIsNotMemberTest extends BaseGroupTest {
+
+    private GroupPage groupPage;
+
+    @Before
+    public void setUp() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login(config.getLogin(), config.getPassword());
+        groupPage = loginPage.openGroupPage(config.getGroupInWichUserIsNotMember());
+    }
+
+    @Test
+    public void userIsAdministratorTest() {
+        Assert.assertTrue(groupPage.checkUserStatus().notInGroup(driver));
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
 }
