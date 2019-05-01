afc273fa980da4140ce31a91e43fc4268dc02f01
Alexey <medvedev.lasta@yandex.ru>
2019-05-01 17:32:07 +0700

    implementation of UserIsMemberTest

--- src/test/java/tests/UserIsMemberTest.java
+++ src/test/java/tests/UserIsMemberTest.java
@@ -1,4 +1,31 @@
 package tests;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import pages.GroupPage;
+import pages.LoginPage;
+
 public class UserIsMemberTest extends BaseGroupTest {
+
+    private GroupPage groupPage;
+
+    @Before
+    public void setUp() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login(config.getLogin(), config.getPassword());
+        groupPage = loginPage.openGroupPage(config.getGroupInWichUserIsMember());
+    }
+
+    @Test
+    public void userIsAdministratorTest() {
+        Assert.assertTrue(groupPage.checkUserStatus().inGroup(driver));
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
 }
